1203c10d0944d2405e09955849be13297bb479b4
Morís Gomez <83724852+morisgomez@users.noreply.github.com>
2022-11-30 10:31:06 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,6 +9,6 @@ int main ()
 
     pointer = &var; //assign the pointer the memory address of var = 30.
     cout << pointer << endl; //points to the address of var. hence, it'll print out a hexadecimal number.
-    cout << *pointer; //prints the value of the address the pointer is pointing too. in this case 30.
+    cout << *pointer; //prints the value of the address the pointer is pointing to. in this case 30.
     return 0;
 }
