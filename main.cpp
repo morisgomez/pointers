46ddd17b8d886b97d1948487f265c96221393d06
Morís Gomez <83724852+morisgomez@users.noreply.github.com>
2022-11-30 10:36:28 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,9 @@ using namespace std;
 int main ()
 {
     int var = 30; //initialize variable with value 30.
-    int *pointer; //declare a pointer variable with name "pointer."
+    int *pointer; //declare a pointer variable with name "pointer." 
+    //line 8 reads: "pointer to int" - we use type int for the pointer var bc we are the storing the address of an int value (30).
+    // if the variable var type were double, then based on the latter logic, the type of the pointer variable would be doubel as well.
 
     pointer = &var; //assign the pointer the memory address of var = 30.
     cout << pointer << endl; //points to the address of var. hence, it'll print out a hexadecimal number.
